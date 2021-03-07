f6252b5203ed1f6ca0b6c8183200b050fa78e38a
Talha <talhaanwar.anwar@gmail.com>
2021-03-07 20:52:28 +0530

    kafka helpers to connect to consumers and endpoint to send message to consumer via producer

--- common/kafka.js
+++ common/kafka.js
@@ -3,20 +3,25 @@ const config = require('../config/config')
 
 const client = new Kafka.KafkaClient({kafkaHost: config.KafkaHost});
 const producer = new Kafka.Producer(client);
-const consumer1 =  new Kafka.Consumer(client,[ {topic: 'FIS', partition: 0}]);
-const consumer2 =  new Kafka.Consumer(client,[ {topic: 'TSYS', partition: 0}]);
-const consumer3 =  new Kafka.Consumer(client,[ {topic: 'AFS', partition: 0}]);
 
+const topicsToCreate = [{
+    topic: 'FIS',
+    partitions: 1,
+    replicationFactor: 1
+    },{
+    topic: 'TSYS',
+    partitions: 1,
+    replicationFactor: 1
+    },{
+    topic: 'AFS',
+    partitions: 1,
+    replicationFactor: 1
+    }
+  ];
 
-consumer1.on('message',(message) =>{
-    console.log(11, message);
-});
-consumer2.on('message',(message) =>{
-    console.log(22, message);
-});
-consumer3.on('message',(message) =>{
-    console.log(33, message);
-});
+let consumer1;
+let consumer2;
+let consumer3;
 
 const api = {
 	createTopics: async topicsToCreate => {
@@ -26,10 +31,27 @@ const api = {
 		});
 	},
 	sendMessageToTopic: async (topic, message) => {
-		producer.send([{topic:topic, messages: 'message'}], (err,data)=>{
+		producer.send([{topic:topic, messages: message}], (err,data)=>{
         	console.log(err, '2 sent');
     	});
 	},
+	attachConsumers: () => {
+		consumer1 =  new Kafka.Consumer(client,[]);
+		consumer2 =  new Kafka.Consumer(client,[]);
+		consumer3 =  new Kafka.Consumer(client,[{topic: 'AFS', partition: 0}]);
+		consumer1.addTopics([ {topic: 'FIS', partition: 0}], () => console.log("topic FIS added"));
+		consumer2.addTopics([ {topic: 'TSYS', partition: 0}], () => console.log("topic TSYS added"));
+		consumer3.addTopics([ {topic: 'AFS', partition: 0}], () => console.log("topic AFS added"));
+		consumer1.on('message',(message) =>{
+		    console.log(11, message);
+		});
+		consumer2.on('message',(message) =>{
+		    console.log(22, message);
+		});
+		consumer3.on('message',(message) =>{
+		    console.log(33, message);
+		});
+	},	
 	doesTopicExists: name => {
 		return new Promise((resolve, reject) => {
 			client.loadMetadataForTopics([name], (err, resp) => {
@@ -42,5 +64,8 @@ const api = {
 		});
 	}
 }
+
+api.createTopics(topicsToCreate);
+api.attachConsumers();
 
 module.exports = api;
--- routes/kafka/kafka.routes.js
+++ routes/kafka/kafka.routes.js
@@ -3,28 +3,46 @@ const kafka = require('../../common/kafka');
 const api = router => {
     const handleRequestForKafka = async (req, res) => {
       const data = req.body;
-      console.log(data);
-      const help = await kafka.doesTopicExists('dd');
-      console.log("help:",help);
-      res.sendStatus(200);
+      let responseCode = 200;
+      let responseMessage = ''
+      let institution = (data.data && data.data.institution) ?
+        data.data.institution : '';
+      const message = (data.data && data.message) ?
+        data.message : '';
+
+      if(institution && message) {
+        institution = institution.toUpperCase();
+        responseMessage = 'Message received for topic: ' + institution;
+        await kafka.sendMessageToTopic(institution, message);
+      } else {
+        responseMessage = 'Please pass both message and institution. eg: ' +
+          JSON.stringify({
+            abc: 'aasd',
+            id: '1234',
+            message: 'message',
+            data: { payment: 'payment 1', institution: 'afs' }
+          });
+        responseCode = 400;
+      }
+      res.status(responseCode).send(responseMessage);
     } 
     const handlecreateTopics = async (req, res) => {
       // create topics
-      const topicsToCreate = [{
-        topic: 'FIS',
-        partitions: 1,
-        replicationFactor: 1
-        },{
-        topic: 'TSYS',
-        partitions: 1,
-        replicationFactor: 1
-        },{
-        topic: 'AFS',
-        partitions: 1,
-        replicationFactor: 1
-        }
-      ];
-      await kafka.createTopics(topicsToCreate);
+      // const topicsToCreate = [{
+      //   topic: 'FIS',
+      //   partitions: 1,
+      //   replicationFactor: 1
+      //   },{
+      //   topic: 'TSYS',
+      //   partitions: 1,
+      //   replicationFactor: 1
+      //   },{
+      //   topic: 'AFS',
+      //   partitions: 1,
+      //   replicationFactor: 1
+      //   }
+      // ];
+      // await kafka.createTopics(topicsToCreate);
       res.sendStatus(200);
     }
     return router
