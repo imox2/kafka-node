07a52048b9b3caecc8072cad5b11c00a21eabed8
Talha <talhaanwar.anwar@gmail.com>
2021-03-07 17:33:32 +0530

    Creater a producer to write to the topic created on localhost

---
+++ app_json.js
@@ -0,0 +1,23 @@
+const jsonData = [{
+  "name":"Talha",
+  "age": 28,
+  "occupation": "Software Engineer",
+  "religion": "Islam",
+  "sports":"football"
+},
+{
+"name":"Sai",
+  "age": 25,
+  "occupation": "Software Tester",
+  "religion": "Hindu",
+  "sports":"Cricket"
+},
+{
+"name":"Faisal",
+  "age": 26,
+  "occupation": "Technical Author & Reviewer",
+  "religion": "Islam",
+  "sports":"Hand-ball"
+}];
+
+module.exports = jsonData;
