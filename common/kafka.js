b3591babc840010778ce019c67d17c09d2d336b7
Talha <talhaanwar.anwar@gmail.com>
2021-03-07 20:02:12 +0530

    Created consumers listening on topic and helper function to send message to consumers

--- common/kafka.js
+++ common/kafka.js
@@ -2,6 +2,21 @@ const Kafka = require('kafka-node');
 const config = require('../config/config')
 
 const client = new Kafka.KafkaClient({kafkaHost: config.KafkaHost});
+const producer = new Kafka.Producer(client);
+const consumer1 =  new Kafka.Consumer(client,[ {topic: 'FIS', partition: 0}]);
+const consumer2 =  new Kafka.Consumer(client,[ {topic: 'TSYS', partition: 0}]);
+const consumer3 =  new Kafka.Consumer(client,[ {topic: 'AFS', partition: 0}]);
+
+
+consumer1.on('message',(message) =>{
+    console.log(11, message);
+});
+consumer2.on('message',(message) =>{
+    console.log(22, message);
+});
+consumer3.on('message',(message) =>{
+    console.log(33, message);
+});
 
 const api = {
 	createTopics: async topicsToCreate => {
@@ -9,6 +24,11 @@ const api = {
 		  // result is an array of any errors if a given topic could not be created
 		  console.log(result, 'topic created successfully');
 		});
+	},
+	sendMessageToTopic: async (topic, message) => {
+		producer.send([{topic:topic, messages: 'message'}], (err,data)=>{
+        	console.log(err, '2 sent');
+    	});
 	},
 	doesTopicExists: name => {
 		return new Promise((resolve, reject) => {
